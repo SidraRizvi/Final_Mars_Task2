f196444723f3ab179c18cefcba10fcb7dd650e9d
SidraRizvi <sidra_riz@yahoo.com>
2021-12-24 23:00:12 +1300

    Join,Login,Add SEller,Delete Seller module automation

---
+++ SpecflowTask2/Hooks/WEBHooks1.cs
@@ -0,0 +1,45 @@
+﻿using BoDi;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace SpecflowTask2.Hooks
+{
+    [Binding]
+    public class WEBHooks1
+    {
+        private readonly IObjectContainer container;
+
+        public WEBHooks1(IObjectContainer container)
+        {
+            this.container = container;
+        }
+
+        [BeforeScenario]
+        public void CreateWebDriver()
+        {
+            ChromeDriver driver = new ChromeDriver();
+
+            // Make 'driver' available for DI
+            container.RegisterInstanceAs<IWebDriver>(driver);
+        }
+
+        [AfterScenario]
+        public void DestroyWebDriver()
+        {
+            var driver = container.Resolve<IWebDriver>();
+
+            if (driver != null)
+            {
+                driver.Quit();
+                driver.Dispose();
+            }
+        }
+
+    }
+}
+
